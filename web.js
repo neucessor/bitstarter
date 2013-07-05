3e1cf7ed99fd64e58cbcae9bae83b32318a91256
neucessor <neu.cessor@gmail.com>
2013-07-05 16:28:56 -0700

    Modify web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var mybuffer = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(mybuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
